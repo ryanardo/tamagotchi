e235669ac3307efd6da6f7101111edc2ae642cc1
Guest User <Guest@Epicodus-3C.local>
2018-02-12 10:54:22 -0800

    Initialize NPM and webpack for Project.

---
+++ webpack.global.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+	entry: {
+		index: './app/src/index.js'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery/dist/jquery.min.js',
+			jQuery: 'jquery'
+		}),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+			template: 'app/html/index.html',
+			title: 'Production'
+		})
+  ],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}]
+	}
+};
