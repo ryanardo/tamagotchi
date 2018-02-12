430cf928e05e0ee8b6261c59e00cedce348fa9f0
Guest User <Guest@Epicodus-3C.local>
2018-02-12 10:58:08 -0800

    Tamagochi.setFood PASS
    Test for passing the Tamagochi.setFood method.

--- test/tamagochi.test.js
+++ test/tamagochi.test.js
@@ -4,4 +4,22 @@ const assert = require('assert');
 
 describe('Tamagotchi', function () {
 
+	let kevin = new Tamagotchi("kevin");
+	let clock = sinon.useFakeTimers();
+
+	beforeEach(function () {
+
+	});
+
+	afterEach(function () {
+		clock.restore();
+	});
+
+	// Calculate seconds in a year
+	describe('setFood', function () {
+		it('[setFood] should return 100.', function () {
+			assert.equal(100, kevin.foodLevel);
+		});
+	});
+
 });
